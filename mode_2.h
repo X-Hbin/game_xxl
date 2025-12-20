6ccce336041cb1000fb5d2d01cd709f5241fc47a
gengqi0807 <gengqi24301033@163.com>
2025-12-21 04:51:53 +0800

    Update mode_2.h

--- mode_2.h
+++ mode_2.h
@@ -10,6 +10,7 @@
 #include <QStack>
 #include <QLabel>
 #include "gameboard.h"
+#include "skilltree.h"
 
 class QGridLayout;
 
@@ -30,9 +31,15 @@ public:
 
     // 【核心】事件过滤器：取代了 handleCellClick
     bool eventFilter(QObject *watched, QEvent *event) override;
+    // 设置技能树
+    void setSkillTree(SkillTree* skillTree) {
+        m_skillTree = skillTree;
+        // 【新增】重置技能使用状态
+        resetSkills();
+    }
 
 signals:
-    void gameFinished();
+    void gameFinished(bool isNormalEnd = false);
 
 private slots:
     void rebuildGrid();
@@ -49,6 +56,7 @@ private:
     QGridLayout *m_gridLayout;
     QVector<QPushButton*> m_cells;
     QSequentialAnimationGroup *m_dropGroup = nullptr;
+    bool m_ultimateBurstActive = false;
 
     // === 旋风模式特有变量 ===
     QLabel *m_selectorFrame; // 2x2 的选择光圈
@@ -99,6 +107,22 @@ private:
     bool findValidMove(int &outR, int &outC); // 查找可消除的旋转点
     void showHint(int r, int c);              // 高亮 2x2 区域
     void stopHint();
+
+    // 新增：技能树引用
+    SkillTree* m_skillTree = nullptr;
+    // 【新增】技能管理器
+    class SkillManager* m_skillManager = nullptr;
+
+    // 【新增】技能激活状态
+    bool m_scoreDoubleActive = false;
+    bool m_colorUnifyActive = false;
+    QTimer* m_skillEffectTimer = nullptr;
+
+    // 【新增】技能相关函数
+    void resetSkills();  // 重置技能状态
+    void onSkillEffectTimeout();  // 技能效果结束
+    void showSkillEndHint(const QString& message);
+    void showTempMessage(const QString& message, const QColor& color);
 };
 
 #endif // MODE_2_H
